e409dd3dc14cb96147a3adfb0ed606ad6e0024c3
victor <victor.kawasaki@wises.com.br>
2023-03-07 00:24:49 -0300

    initial comminit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/main/main.component.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent {
+
+  // masculino: 66 + (13,7 x peso) + (5 x altura) - (6,8 x idade)
+  // feminino: 655 + (9,6 x peso) + (1,8 x altura) - (4,7 x idade)
+  // niveis atividade
+  // 1.2 - sedentário
+  // 1.375 - pouco ativo
+  // 1.55 - moderadamente ativo
+  // 1.725 - muito ativo
+  // 1.9 - extremamente ativo
+
+  opcoes = [
+    { value: '1.2', viewValue: 'Sedentário' },
+    { value: '1.375', viewValue: 'Pouco Ativo' },
+    { value: '1.55', viewValue: 'Moderadamente Ativo' },
+    { value: '1.725', viewValue: 'Muito Ativo' },
+    { value: '1.9', viewValue: 'Extremamente Ativo' }
+  ]
+
+  sexo:any = 'M';
+  peso:any = 0;
+  altura:any = 0;
+  idade:any = 0;
+  nivelAtividade:any = "";
+  resultado = 0;
+
+  selecaoSexo($event:any) {
+    this.sexo = $event.target.value;
+  }
+
+  calcular() {
+    let pesocalc = 13.7 * parseFloat(this.peso);
+    let alturacalc = 5 * parseFloat(this.altura);
+    let idadecalc = 6.8 * parseFloat(this.idade);
+
+    console.log(pesocalc, alturacalc, idadecalc);
+
+    if (this.sexo == 'M') {
+      this.resultado = 66 + (13.7 * this.peso) + (5 * this.altura) - (6.8 * this.idade);
+    } else {
+      this.resultado = 655 + (9.6 * this.peso) + (1.8 * this.altura) - (4.7 * this.idade);
+    }
+
+    if (this.nivelAtividade == "") {
+      alert("Selecione um nível de atividade");
+      this.resultado = 0;
+    }
+    else {
+
+      this.resultado = this.resultado * this.nivelAtividade;
+    }
+
+  }
+}
